ebe672c6e87950028b7750a9f8a3e613f92e3ea4
Matias Seijas <matiseijas@gmail.com>
2018-08-27 21:55:44 -0400

    Deployed contracts to Rinkeby

--- truffle.js
+++ truffle.js
@@ -1,6 +1,9 @@
 require('babel-register')
 process.env.NODE_ENV = process.env.NODE_ENV || 'development' // Required by babel-preset-react-app
 
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const mnemonic = '## REPLACE FOR DEPLOYMENT ##'
+
 const mochaSettings = () => {
   if (!process.env.GAS_REPORTER) return {}
 
@@ -18,6 +21,12 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, '## REPLACE FOR DEPLOYMENT ##', 0)
+      },
+      network_id: 4
     }
   },
   mocha: mochaSettings()
